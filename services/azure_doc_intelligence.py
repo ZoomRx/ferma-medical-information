61900a4a9e130c02256985ca9d432fba050d6dd9
Beril <beril.varghese@zoomrx.com>
2024-06-28 16:58:05 +0530

    Added Markdown for Tables

--- services/azure_doc_intelligence.py
+++ services/azure_doc_intelligence.py
@@ -82,7 +82,7 @@ class AzureDocIntelligence():
 
             if start_position > next_chunck_position:
                 if metadata["role"] == "table":
-                    #content = Helpers.json_to_markdown(table=raw_output_obj.tables[metadata["table_index"]])
+                    content = self.json_to_markdown(table=raw_output_obj.tables[metadata["table_index"]])
                     next_chunck_position = metadata["end_position"]
                 elif metadata["role"] == "figure":
                     content = raw_output_obj.content[start_position:metadata["end_position"]]
@@ -108,3 +108,39 @@ class AzureDocIntelligence():
         page_df = pd.concat([page_df, pd.DataFrame([page_row])], ignore_index=True)
         cluster_df = pd.merge(paragraph_df, page_df, on='page_no')
         return cluster_df
+
+    @staticmethod
+    def json_to_markdown(*, table: Dict[str, Any]) -> str:
+        markdown_text = ""
+        headers = [""] * table["columnCount"]
+        headers_present = 0
+
+        for cell in table['cells']:
+            content = cell['content'].replace('\n', '')
+            content = content.replace(':selected:', '')
+            content = content.replace(':unselected:', '')
+            if cell.get('kind', "") == 'columnHeader' and cell['rowIndex'] == 0:
+                headers[cell['columnIndex']] = content
+                headers_present = 1
+
+        table_matrix = [[""] * table["columnCount"] for _ in range(table["rowCount"])]
+
+        for cell in table['cells']:
+            content = cell['content'].replace('\n', '')
+            content = content.replace(':selected:', '')
+            content = content.replace(':unselected:', '')
+            if not (cell.get('kind', "") == 'columnHeader' and cell['rowIndex'] == 0):
+                table_matrix[cell['rowIndex'] - headers_present][cell['columnIndex']] = content
+
+        for i in range(table["rowCount"] - headers_present):
+            markdown_text += "|"
+            for j in range(table["columnCount"]):
+                markdown_text += table_matrix[i][j] + "|"
+            markdown_text += "\n"
+
+        pattern = "|" * (table["columnCount"] + 1) + "\n"
+        markdown_text = markdown_text.replace(pattern, "")
+        markdown_text = "|" + "|".join(['-'] * table['columnCount']) + "|\n" + markdown_text
+        markdown_text = "|" + "|".join(headers) + "|\n" + markdown_text
+        markdown_text = f"{table['caption']['content']}\n{markdown_text}" if table.get('caption') else markdown_text
+        return markdown_text
